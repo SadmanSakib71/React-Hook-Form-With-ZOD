4cb4a81fa324e3316638d2aa45193fedb631040d
Sadman Sakib <1550504940+SadmanSakib71@users.noreply.github.com>
2026-02-01 15:55:42 +0600

    pass error message outside required field

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, type SubmitHandler } from "react-hook-form";
+import "./App.css";
+type FormFields = {
+  email: string;
+  password: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          })}
+          type="text"
+          placeholder="Email"
+        />
+        {errors.email && <div className="text-red-500">My Name is khan</div>}
+        <label>Password</label>
+        <input
+          {...register("password", { required: true, minLength: 6 })}
+          type="password"
+          placeholder="password"
+        />
+        <input type="submit" />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
